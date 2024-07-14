d24867b5c9c0ba69604e1bec7df73b2bdabd5a69
shinshin86 <beagles1986@gmail.com>
2024-07-14 16:14:37 +0900

    add web_accessible_resources and fix url

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import manifest from "./manifest.json" assert { type: "json" };
 
 export default {
   plugins: [crx({ manifest })],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        content: "src/content-script.ts",
+      },
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+  },
 };
